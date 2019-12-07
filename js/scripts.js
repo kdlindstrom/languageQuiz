9a7602c8b02e24d8b7971acbd6daadc6d42ca756
Kyle Lindstrom <kyledlindstrom@gmail.com>
2019-12-06 20:53:31 -0800

    add some more questions and console.log debugging

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,7 @@ $(document).ready(function(){
   $("#survey form").submit(function(event){
   event.preventDefault();
   var devType = $("#devType").val();
-  var age = $("#age").val();
-  var animal = $("#animal").val();
+  var foodType = $("#foodType").val();
   var javaScore=0;
   var pythonScore=0;
   var swiftScore=0;
@@ -16,14 +15,36 @@ $(document).ready(function(){
     }else if (devType ==='iOS apps'){
       swiftScore +=1;
     }
-    alert(javaScore);
+
+    if (foodType === 'burritos') {
+      javaScore +=1;
+    }else if (foodType==='pizza'){
+      pythonScore +=1;
+    }else if (foodType ==='pizza burritos'){
+      alert("I don't think that's a real food, and if it is it probably shouldn't be.");
+    }
+
+    if (artistType === 'Ja Rule') {
+      javaScore +=1;
+    }else if (artistType==='Whitesnake'){
+      pythonScore +=1;
+    }else if (artistType ==='Taylor Swift'){
+        swiftScore +=1;
+    }
+    console.log("java" +javaScore);
+    console.log("python"+pythonScore);
+    console.log("swift"+swiftScore);
+
+
     if (javaScore>pythonScore && javaScore>swiftScore){
         $("#output").text("You should learn java.");
     }else if(pythonScore>javaScore && pythonScore>swiftScore){
         $("#output").text("You should learn python.");
-    }else {
+    }else if(swiftScore>javaScore && swiftScore>pythonScore){
         $("#output").text("You should learn swift.");
-    }
+    }else(
+        alert("please complete the form")
+    );
 
   });
 });
